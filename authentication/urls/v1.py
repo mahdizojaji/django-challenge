5d4852471298314a2a48ee8f7a89e746ff827c29
Mahdi Zojaji <mmahdizojajim@gmail.com>
2022-07-08 14:46:12 +0430

    Add refresh API to refresh JWT access token

--- authentication/urls/v1.py
+++ authentication/urls/v1.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from dj_rest_auth.jwt_auth import get_refresh_view
+
 from ..views import SendCodeAPIView, LoginAPIView
 
 urlpatterns = [
     path("send_code/", SendCodeAPIView.as_view(), name="send_code_v1"),
     path("login/", LoginAPIView.as_view(), name="login_v1"),
+    path("refresh/", get_refresh_view().as_view(), name="token_refresh_v1"),
 ]
