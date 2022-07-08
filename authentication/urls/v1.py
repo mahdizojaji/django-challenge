3027a7b4a5c3376836544fb44430e6e24c0981d2
Mahdi Zojaji <mmahdizojajim@gmail.com>
2022-07-08 15:24:26 +0430

    Add verify API to verify JWT tokens

--- authentication/urls/v1.py
+++ authentication/urls/v1.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenVerifyView
 from dj_rest_auth.jwt_auth import get_refresh_view
 
 from ..views import SendCodeAPIView, LoginAPIView
@@ -8,4 +9,5 @@ urlpatterns = [
     path("send_code/", SendCodeAPIView.as_view(), name="send_code_v1"),
     path("login/", LoginAPIView.as_view(), name="login_v1"),
     path("refresh/", get_refresh_view().as_view(), name="token_refresh_v1"),
+    path("verify/", TokenVerifyView.as_view(), name="token_verify_v1"),
 ]
